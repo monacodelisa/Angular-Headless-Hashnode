6295d922d2075251dfe29fed7c61e6a5c86c57f0
Esther White <monacodelisa@gmail.com>
2024-02-14 10:42:06 +0200

    feat(footer): clog title link colors light mode

--- angular-v16/src/app/components/footer/footer.component.ts
+++ angular-v16/src/app/components/footer/footer.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  blogName = "Blog Name";
+export class FooterComponent implements OnInit, OnDestroy {
+  blogInfo: any;
+  blogName = "";
   date = new Date().getFullYear();
+  private querySubscription?: Subscription;
+  blogService: BlogService = inject(BlogService);
+
+  ngOnInit(): void {
+    this.querySubscription = this.blogService.getBlogInfo().subscribe((data) => {
+      this.blogInfo = data;
+      this.blogName = this.blogInfo.title;
+    });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription?.unsubscribe();
+  }
 }
