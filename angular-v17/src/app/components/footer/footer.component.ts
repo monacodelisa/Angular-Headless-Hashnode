9b0f7956d1334c690ecd319af13ac03ba23a9aa2
Esther White <monacodelisa@gmail.com>
2024-02-15 08:24:18 +0200

    feat(footer):component created with required elements

---
+++ angular-v17/src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  blogName = "Angular v17";
+  date = new Date().getFullYear();
+}
