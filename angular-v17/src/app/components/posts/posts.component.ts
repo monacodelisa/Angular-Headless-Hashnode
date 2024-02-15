c11b13ab4cf20bdca6398be267a6c798486e92ae
Esther White <monacodelisa@gmail.com>
2024-02-15 11:02:03 +0200

    feat(posts): apollo angular v17 graphql blog posts

--- angular-v17/src/app/components/posts/posts.component.ts
+++ angular-v17/src/app/components/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from "apollo-angular";
+import { map } from 'rxjs';
+import { GET_POSTS } from '../../graphql.operations';
 
 @Component({
   selector: 'app-posts',
@@ -7,15 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
-  posts = [
-    {
-      title: 'Post 1',
-      content: 'This is the first post'
-    },
-    {
-      title: 'Post 2',
-      content: 'This is the second post'
-    }
-  ];
+export class PostsComponent implements OnInit {
+  posts: any;
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit(): void {
+    this.apollo
+    .watchQuery<any>({
+      query: GET_POSTS,
+    })
+    .valueChanges.pipe(map(({ data }) => data.publication.posts.edges)).subscribe(
+      (posts) => {
+        this.posts = posts;
+        console.log(posts);
+      }
+    );
+  }
 }
---
+++ angular-v17/src/app/graphql.operations.ts
@@ -0,0 +1,50 @@
+import { gql } from "apollo-angular";
+
+export const BLOG_HOST = "angular-headless.hashnode.dev";
+
+export const GET_BLOG_INFO = gql`
+query Publication {
+  publication(host: "${BLOG_HOST}") {
+    title,
+    links {
+      twitter
+      instagram
+      github
+      website
+      hashnode
+      youtube
+      dailydev
+      linkedin
+      mastodon
+    }
+    followersCount,
+    url
+  }
+}
+`;
+
+export const GET_POSTS = gql`
+query Publication {
+  publication(host: "${BLOG_HOST}") {
+    id,
+    isTeam,
+    title,
+    posts(first: 10) {
+      edges {
+        node {
+          id,
+          slug,
+          coverImage{
+            url
+          }
+          title,
+          brief,
+          content {
+            html
+          }
+        }
+      }
+    }
+  }
+}
+`;
