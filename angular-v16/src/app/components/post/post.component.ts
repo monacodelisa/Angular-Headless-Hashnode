3b969aef76a78728a67de770e63f9511c3fde48e
Esther White <monacodelisa@gmail.com>
2024-02-16 12:06:02 +0200

    feat(post): basic styling and layout of single post

--- angular-v16/src/app/components/post/post.component.ts
+++ angular-v16/src/app/components/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject } from "@angular/core";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { ActivatedRoute } from "@angular/router";
 import { BlogService } from "src/app/services/blog.service";
 
@@ -9,14 +10,17 @@ import { BlogService } from "src/app/services/blog.service";
 })
 export class PostComponent implements OnInit {
 	post: any | undefined;
+  postContent: SafeHtml = '';
 	blogService: BlogService = inject(BlogService);
 	route: ActivatedRoute = inject(ActivatedRoute);
+  sanitizer: DomSanitizer = inject(DomSanitizer);
 
   ngOnInit() {
     const postSlug = String(this.route.snapshot.params['slug']);
 
     this.blogService.getPostBySlug(postSlug).subscribe((data) => {
       this.post = data;
+      this.postContent = this.sanitizer.bypassSecurityTrustHtml(data.content.html);
       console.log(this.post);
     });
   }
