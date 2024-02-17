6af1b4dacaa873216b30a15859e1dd9e8acaa8e5
Esther White <monacodelisa@gmail.com>
2024-02-17 13:56:21 +0200

    feat(presentation site): rauting, theme, styling

--- angular-hashnode/src/app/app.component.ts
+++ angular-hashnode/src/app/app.component.ts
@@ -1,52 +1,68 @@
-import { Component, ChangeDetectorRef, OnDestroy } from "@angular/core";
-import {MediaMatcher} from '@angular/cdk/layout';
-import { RouterOutlet } from "@angular/router";
+import { Component, ChangeDetectorRef, OnInit, OnDestroy, inject } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
+import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
-import { MatListModule } from "@angular/material/list";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatIconModule } from "@angular/material/icon";
+import { NavItem } from "./models/nav-item";
+import { ThemeService } from "./services/theme.service";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
 	imports: [
 		RouterOutlet,
+		RouterLink,
+		RouterLinkActive,
 		HomeComponent,
 		FooterComponent,
-		MatToolbarModule,
-		MatButtonModule,
-		MatIconModule,
 		MatSidenavModule,
 		MatListModule,
+		MatToolbarModule,
+		MatButtonModule,
+    MatSlideToggleModule,
+		MatIconModule,
 	],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-	title = "Angular + Hashnode";
-  snavIsOpen = true;
-  mobileQuery: MediaQueryList;
+export class AppComponent implements OnInit, OnDestroy {
+	title = "Angular & Hashnode";
+	snavIsOpen = true;
+	mobileQuery: MediaQueryList;
+  themeService: ThemeService = inject(ThemeService);
 
-  navItems = [
-    { name: "Home", route: "home", icon: "home" },
-    { name: "Demos", route: "demos", icon: "widgets" },
-    { name: "About", route: "about", icon: "info" },
-    { name: "Contact", route: "contact", icon: "email" },
-  ];
+	navItems: NavItem [] = [
+		{ name: "Home", route: "home", icon: "home" },
+		{ name: "Demos", route: "demos", icon: "widgets" },
+		{ name: "About", route: "about", icon: "info" },
+		{ name: "Contact", route: "contact", icon: "email" },
+	];
 
-  private _mobileQueryListener: () => void;
+	private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+	constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+		this.mobileQuery = media.matchMedia("(max-width: 600px)");
+		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+		this.mobileQuery.addListener(this._mobileQueryListener);
+	}
+
+
+  ngOnInit(): void {
+
   }
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+  toggleTheme() {
+    this.themeService.updateTheme();
   }
+
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this._mobileQueryListener);
+	}
 }
--- angular-v17/src/app/components/header/header.component.ts
+++ angular-v17/src/app/components/header/header.component.ts
@@ -31,7 +31,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
       this.blogInfo = data;
       this.blogName = this.blogInfo.title;
       this.blogSocialLinks = data.links;
-      console.log(this.blogSocialLinks);
     });
   }
 
