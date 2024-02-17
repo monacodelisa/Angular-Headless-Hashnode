96cd9610e24a3923ae2b9b17ff29c45ffc6d2b26
Esther White <monacodelisa@gmail.com>
2024-02-17 15:41:57 +0200

    feat(presentation site): more work on it

--- angular-hashnode/src/app/components/home/home.component.ts
+++ angular-hashnode/src/app/components/home/home.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: "app-home",
+	standalone: true,
+	imports: [],
+	templateUrl: "./home.component.html",
+	styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
+  videoVolume = 0;
 
+	goals = [
+    {"name":"Develop a flexible template for Angular integration with headless Hashnode."},
+    {"name":"Connect the Angular app to Hashnode's GraphQL database to retrieve posts and blog data."},
+    {"name":"Support Angular versions 16 and 17 with Angular Material UI."},
+    {"name":"Support Angular versions 16 and 17 with PrimeNG."},
+    {"name":"Responsive layout improvements for different devices."},
+  ];
+	features = [
+    {"name":"v16 & v17",
+    "linkName":"Angular",
+    "link": "https://angular.dev/"},
+    {"name":"GraphQL Client for Angular",
+    "linkName":"Appolo Angular",
+     "link": "https://the-guild.dev/graphql/apollo-angular/docs"},
+    {"name":"",
+    "linkName":"Font Awesome Icons",
+     "link": "https://fontawesome.com/"},
+     {"name":"",
+    "linkName":"Google Material Symbols and Icons",
+     "link": "https://fonts.google.com/icons"},
+     {"name":"",
+    "linkName":"Google Fonts",
+     "link": "https://fonts.google.com/"},
+  ];
 }
