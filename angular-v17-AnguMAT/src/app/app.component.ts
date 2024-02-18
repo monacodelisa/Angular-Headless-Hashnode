43208b6782e89993334dbece8ecce903938ba168
Ali7040 <ha4219466@gmail.com>
2024-02-18 20:26:03 +0500

    added themeToggle functionality.

--- angular-v17-AnguMAT/src/app/app.component.ts
+++ angular-v17-AnguMAT/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-v17-AnguMAT';
+  themeService: ThemeService = inject(ThemeService);
 }
