b1b9e68dffe5e3ed7efd86ecedcc2758deb46bca
Ali7040 <ha4219466@gmail.com>
2024-02-20 16:36:26 +0500

    update the footer coponent

--- angular-v17-AnguMAT/src/app/components/footer/footer.component.ts
+++ angular-v17-AnguMAT/src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
