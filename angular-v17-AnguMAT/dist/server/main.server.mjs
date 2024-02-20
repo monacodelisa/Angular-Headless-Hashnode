c3b7537865de9715c886feabb540171f882d5f92
Esther White <monacodelisa@gmail.com>
2024-02-20 21:37:43 +0200

    new build

---
+++ angular-v17-AnguMAT/dist/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-XYZ4WWRD.mjs";import"./chunk-CSKTMY2N.mjs";import"./chunk-QBWTLP3P.mjs";import"./chunk-5BV57IXF.mjs";import"./chunk-SY3FR42F.mjs";import"./chunk-Q5XSRLOX.mjs";import"./chunk-25CKIFGM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
